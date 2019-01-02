c795c0f72a292a9e8c868df989b0edc2f1a8924f
knyg <kevin1992yang@gmail.com>
2019-01-01 17:15:41 -0800

    starting grid

---
+++ src/Components/Grid.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+export default class Grid extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div className="container-fluid grid">
+
+                <div className="row week-1">
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday AM</div>
+                </div>
+
+                <div className="row week-1">
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday PM</div>
+                </div>
+
+
+                <div className="row week-2">
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday AM</div>
+                </div>
+
+                <div className="row week-2">
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday PM</div>
+                </div>
+
+
+                <div className="row week-3">
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday AM</div>
+                </div>
+
+                <div className="row week-3">
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday PM</div>
+                </div>
+
+
+                <div className="row week-4">
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday AM</div>
+                    <div className="days col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday AM</div>
+                </div>
+
+                <div className="row week-4">
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Monday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Tuesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Wednesday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Thursday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Friday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Saturday PM</div>
+                    <div className="nights col-sm-1 col-md-1 col-lg-1 col-xl-1">Sunday PM</div>
+                </div>
+
+            </div>
+
+        );
+    };
+};
