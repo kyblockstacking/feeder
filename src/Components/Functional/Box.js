e5b93b747f4ce8730c0510bd7d0d219a6c03d884
knyg <kevin1992yang@gmail.com>
2019-01-03 22:33:08 -0800

    able to change custom element property attributes

--- src/Components/Functional/Box.js
+++ src/Components/Functional/Box.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Box = (props) => {
-    return <div style={{ background: props.color, border: '2px solid black', textAlign: 'center' }} className='col-sm-1 col-md-1 col-lg-1 col-xl-1'>{props.text}</div>
-}
+    return <div onClick={props.onclick} color={props.color} style={{ background: props.color, border: '2px solid black', textAlign: 'center' }} className='col-sm-1 col-md-1 col-lg-1 col-xl-1'>{props.text}</div>
+};
 
 export default Box;
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -10,65 +10,24 @@ export default class Grid extends Component {
         date: new Date().getDate(),
         day: new Date().getDay(),
 
+
     };
 
     componentDidMount() {
         this.ConvertDayToText();
         this.ConveryMonthToText();
-    }
-
-    ConvertDayToText = () => {
-        switch (this.state.day) {
-            case 0: this.setState({ day: 'Sunday' })
-                break;
-            case 1: this.setState({ day: 'Monday' })
-                break;
-            case 2: this.setState({ day: 'Tuesday' })
-                break;
-            case 3: this.setState({ day: 'Wednesday' })
-                break;
-            case 4: this.setState({ day: 'Thursday' })
-                break;
-            case 5: this.setState({ day: 'Friday' })
-                break;
-            case 6: this.setState({ day: 'Saturday' })
-                break;
-            default:
-                console.log('What day is it?')
-                break;
-        };
     };
 
-    ConveryMonthToText = () => {
-        switch (this.state.month) {
-            case 0: this.setState({ month: 'January' })
-                break;
-            case 1: this.setState({ month: 'February' })
-                break;
-            case 2: this.setState({ month: 'March' })
-                break;
-            case 3: this.setState({ month: 'April' })
-                break;
-            case 4: this.setState({ month: 'May' })
-                break;
-            case 5: this.setState({ month: 'June' })
-                break;
-            case 6: this.setState({ month: 'July' })
-                break;
-            case 7: this.setState({ month: 'August' })
-                break;
-            case 8: this.setState({ month: 'September' })
-                break;
-            case 9: this.setState({ month: 'October' })
-                break;
-            case 10: this.setState({ month: 'November' })
-                break;
-            case 11: this.setState({ month: 'December' })
-                break;
-            default:
-                console.log('What month is it?')
-                break;
-        };
+    handleClick = (event) => {
+
+        if (event.target.getAttribute('color') === 'red') {
+            event.target.setAttribute('color', 'green');
+        }
+
+        else if (event.target.getAttribute('color') === 'green') {
+            event.target.setAttribute('color', 'red');
+        }
+
     };
 
     render() {
@@ -79,7 +38,7 @@ export default class Grid extends Component {
 
                 <div className='week'>Week 1</div>
                 <div className='row week-1'>
-                    <Box text={['Monday', < br />, 'PM']} />
+                    <Box onclick={this.handleClick} id='box' color='red' text={['Monday', < br />, 'PM']} />
                     <Box text={['Tuesday', < br />, 'PM']} />
                     <Box text={['Wednesday', < br />, 'PM']} />
                     <Box text={['Thursday', < br />, 'PM']} />
@@ -186,4 +145,60 @@ export default class Grid extends Component {
 
         );
     };
+
+
+    ConvertDayToText = () => {
+        switch (this.state.day) {
+            case 0: this.setState({ day: 'Sunday' })
+                break;
+            case 1: this.setState({ day: 'Monday' })
+                break;
+            case 2: this.setState({ day: 'Tuesday' })
+                break;
+            case 3: this.setState({ day: 'Wednesday' })
+                break;
+            case 4: this.setState({ day: 'Thursday' })
+                break;
+            case 5: this.setState({ day: 'Friday' })
+                break;
+            case 6: this.setState({ day: 'Saturday' })
+                break;
+            default:
+                console.log('What day is it?')
+                break;
+        };
+    };
+
+    ConveryMonthToText = () => {
+        switch (this.state.month) {
+            case 0: this.setState({ month: 'January' })
+                break;
+            case 1: this.setState({ month: 'February' })
+                break;
+            case 2: this.setState({ month: 'March' })
+                break;
+            case 3: this.setState({ month: 'April' })
+                break;
+            case 4: this.setState({ month: 'May' })
+                break;
+            case 5: this.setState({ month: 'June' })
+                break;
+            case 6: this.setState({ month: 'July' })
+                break;
+            case 7: this.setState({ month: 'August' })
+                break;
+            case 8: this.setState({ month: 'September' })
+                break;
+            case 9: this.setState({ month: 'October' })
+                break;
+            case 10: this.setState({ month: 'November' })
+                break;
+            case 11: this.setState({ month: 'December' })
+                break;
+            default:
+                console.log('What month is it?')
+                break;
+        };
+    };
+
 };
